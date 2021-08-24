7f997be17d8beed56ff1f02826e45db511e9d5a6
Sarah LESCURE <slescure@e1r5p6.42.fr>
2021-08-24 14:27:13 +0200

    avancement sur server.c

--- server.c
+++ server.c
@@ -8,10 +8,21 @@ void	print_message(char *msg)
 	free(msg);
 }
 
+void	signal_received()
+{
+	printf("SIGNAL RECEIVED\n");
+}
+
 int	main(void)
 {
+	pid_t my_pid;
+
 	/* recuperer le PID */
-	
+	my_pid = getpid();
+	printf("PID = %d\n", my_pid);
+    signal(SIGUSR1, );
+    return (0);
+
 	while (1)
 		pause(); // attend le signal du client
 }
